c3c3fef21f6802a008c65ab68cc412d99d2b118e
Jay Suryawansh <jay@example.com>
2026-07-14 18:28:17 +0530

    chore: bump version to 0.15.1, replace bcrypt with bcryptjs, and upgrade multer to v2.2.0

--- src/tools/security/crypto.ts
+++ src/tools/security/crypto.ts
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken'
-import bcrypt from 'bcrypt'
+import bcrypt from 'bcryptjs'
 import { createHmac, createCipheriv, createDecipheriv, randomBytes, scrypt, createHash } from 'crypto'
 import { promisify } from 'util'
 
